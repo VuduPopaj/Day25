3b63d54a399de9444577ca7de194229ba4ad222c
VuduPopaj <adnanduljkovic@hotmail.com>
2021-04-01 20:22:18 +0200

    ex05 i neke ispravke

--- ex04/src/GetRequestSetHeaders.js
+++ ex04/src/GetRequestSetHeaders.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class GetRequestSetHeaders extends React.Component{
+class GetRequestSetHeaders extends React.Component {
     constructor(props) {
         super(props);
         this.state = { totalReactPackages: null };
@@ -19,7 +19,7 @@ class GetRequestSetHeaders extends React.Component{
                 <h5 className="card-header">GET Request with Set Headers</h5>
                 <div className="card-body">Total packages: {totalReactPackages}</div>
             </div>
-        )
+        );
      }
 }
 
